92df1647da1e76d227919844cb3e42c092cdbf61
willan123 <willan@mac.com>
2016-11-27 06:03:08 -0500

    Heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,13 +13,13 @@ class MessagesController < ApplicationController
       sms = @client.messages.create(
         from: Rails.application.secrets.twilio_number,
         to: from_number,
-        body: "200 University Ave - William Nippard - +16136989868 \n 151 Charles Street West - Jacob Willemsma - +15192220686"
+        body: "Address - Barsha Ali - 880-645-3363 \nAddress - Dina Hasan - 880-334-4444 \nAddress - Hridi Chakma - 880-978-4425 \nAddress - Ojana Thakur - 880-623-7112"
       )
     elsif (message_body == "Schedule" || message_body == "schedule")
       sms = @client.messages.create(
         from: Rails.application.secrets.twilio_number,
         to: from_number,
-        body: "Example schedule"
+        body: "Wednesday, November 28, 2016 - Address - Name - Third Trimester Check-Up \nWednesday, November 30, 2016 - Address - Name - Pre Birth Examination"
       )
     end
   end
@@ -29,7 +29,7 @@ class MessagesController < ApplicationController
     sms = @client.messages.create(
       from: Rails.application.secrets.twilio_number,
       to: '+16136989868',
-      body: "Textingggg"
+      body: "Example text send."
     )
     redirect_to(:back)
   end
