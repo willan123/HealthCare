21343a5916e4376e809773e197babb18e620aac0
willan123 <willan@mac.com>
2016-11-27 08:04:27 -0500

    Removed login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,18 @@
 class DashboardController < ApplicationController
-  # before_action :authenticate_user!
+   #before_action :authenticate_user!
 
   def index
-    @patients = current_user.patients
-    @low_risk = @patients.where(:condition => 0).count
-    @mild_risk = @patients.where(:condition => 1).count
-    @critical = @patients.where(:condition => 2).count
-    @seen = @patients.where(:seen => true).count
+    #@patients = current_user.patients
+    #@low_risk = @patients.where(:condition => 0).count
+    #@mild_risk = @patients.where(:condition => 1).count
+    #@critical = @patients.where(:condition => 2).count
+    #@seen = @patients.where(:seen => true).count
   end
 
   def schedule
   end
 
   def patients
-    @patients = current_user.patients
+    #@patients = current_user.patients
   end
 end
