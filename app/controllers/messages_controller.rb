703475d4bd29d7a23e9f826127cbb3cf387a769b
willan123 <willan@mac.com>
2016-11-27 10:11:32 -0500

    Texting update.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,17 +9,17 @@ class MessagesController < ApplicationController
     message_body = params["Body"]
     from_number = params["From"]
     boot_twilio
-    if (message_body == "Critical" || message_body == "critical")
+    if (message_body.downcase == "critical")
       sms = @client.messages.create(
         from: Rails.application.secrets.twilio_number,
         to: from_number,
-        body: "Address - Barsha Ali - 880-645-3363 \nAddress - Dina Hasan - 880-334-4444 \nAddress - Hridi Chakma - 880-978-4425 \nAddress - Ojana Thakur - 880-623-7112"
+        body: "34 Jamalpur Street - Barsha Ali - 880-645-3363\n\n155 Halima Tower - Dina Hasan - 880-334-4444\n\n6572 Nizam Road - Hridi Chakma - 880-978-4425\n\n74 Koula Road - Ojana Thakur - 880-623-7112"
       )
-    elsif (message_body == "Schedule" || message_body == "schedule")
+    elsif (message_body.downcase == "schedule")
       sms = @client.messages.create(
         from: Rails.application.secrets.twilio_number,
         to: from_number,
-        body: "Wednesday, November 28, 2016 - Address - Name - Third Trimester Check-Up \nWednesday, November 30, 2016 - Address - Name - Pre Birth Examination"
+        body: "Monday, November 28 at 12:30 - 1 Minto Road, Dhaka - Disha Das - Third Trimester Check-Up\n\nWednesday, November 30 at 8:30 - Jadabpur Berbari Community Clinic - Kwan Dutta - Pre Birth Examination\n\nMonday, December 5 - Krishnapur Community Clinic - Fahmida Barua - Due Date"
       )
     end
   end
